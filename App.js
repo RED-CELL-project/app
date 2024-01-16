f6c234322e6e0827db7c1a942f49bbb181228744
Nyota0 <antoine.buret254@gmail.com>
2024-01-16 17:19:51 +0100

    code du jour

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const PRIMARY_COLOR = '#F1AEAE';
+const SECONDARY_COLOR = '#A80B29';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenue dans l'application RED CELL !</Text>
-      <Text>Test modif Antoine 3</Text>
+      <Header
+        leftComponent={{ icon: 'menu', color: SECONDARY_COLOR, size: 65, }}
+        centerComponent={{ text: 'RED CELL', style: { color: SECONDARY_COLOR, fontSize: 36, fontWeight: 'bold' } }}
+        containerStyle={styles.header}
+      />
+      <Button
+        title="Télécharger et envoyer mes données"
+        titleStyle={{ color: 'black' }}
+        type="clear"
+        buttonStyle={styles.boutons}
+        icon={
+          <Icon
+            name="arrow-right"
+            size={30}
+            color="black"
+          />
+        }
+        iconLeft
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +38,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: PRIMARY_COLOR,
+    height: 110,
+    marginBottom: 50,
+  },
+  boutons: {
+    backgroundColor: PRIMARY_COLOR,
   },
 });
