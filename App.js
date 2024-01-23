11aa282a3fbd870b6e6004539e9259f388f1b645
mostmura <71722763+mostmura@users.noreply.github.com>
2024-01-23 10:12:47 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Header, Icon } from "react-native-elements";
+import { Button } from "react-native-elements";
 
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
@@ -18,22 +19,75 @@ export default function App() {
       style: {
         color: "#fff",
         fontSize: 36,
-        fontWeight: "bold"
+        fontWeight: "bold",
       }
     }}
     centerContainerStyle={{}}
     containerStyle={{ width: "100%" }}
-    leftComponent={{
-      icon: "menu",
-      color: "#fff",
-      size: 50
-    }}
-    leftContainerStyle={{}}
-    placement="center"
-    rightContainerStyle={{}}
-    statusBarProps={{}}
-  />
+    leftComponent={
+      <Button
+      buttonStyle={{
+        width: 60,
+        height: 60,
+        backgroundColor: "#F1AEAE"
+      }}
+      disabledStyle={{
+        borderWidth: 2,
+        borderColor: "#00F"
+      }}
+      disabledTitleStyle={{ color: "#00F" }}
+      icon={<Icon name="menu" size={50} color="#FFF" />}
+      iconContainerStyle={{ background: "#000" }}
+      loadingProps={{ animating: true }}
+      loadingStyle={{}}
+      onPress={() => alert("click")}
+      titleProps={{}}
+    />
+}
+leftContainerStyle={{width: 50, height: 50}}
+placement="center"
+rightContainerStyle={{}}
+statusBarProps={{}}
+/>
+<View
+  style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 80}}
+>
+  {/* All corners */}
+  <TouchableOpacity style={{backgroundColor: '#F1AEAE', padding: 30, borderRadius: 8, width: 300, height: 120, alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={{color: '#fff', fontSize: 24}}>Enregistrement</Text>
+  </TouchableOpacity>
+  
+</View>
+<View
+  style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 80}}
+>
+  {/* All corners */}
+  <TouchableOpacity style={{backgroundColor: '#F1AEAE', padding: 30, borderRadius: 8, width: 300, height: 120, alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={{color: '#fff', fontSize: 24}}>Historique</Text>
+  </TouchableOpacity>
+  
+</View>
+<View
+  style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 80}}
+>
+  {/* All corners */}
+  <TouchableOpacity style={{backgroundColor: '#F1AEAE', padding: 30, borderRadius: 8, width: 300, height: 120, alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={{color: '#fff', fontSize: 24}}>Pilulier</Text>
+  </TouchableOpacity>
+  
+</View>
+<View
+  style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 80}}
+>
+  {/* All corners */}
+  <TouchableOpacity style={{backgroundColor: '#F1AEAE', padding: 30, borderRadius: 8, width: 300, height: 120, alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={{color: '#fff', fontSize: 24}}>Tutoriels</Text>
+  </TouchableOpacity>
+  
+</View>
   </SafeAreaView>
+  
+
 
   );
 }
