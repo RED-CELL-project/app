33eed417d6d8f0cc9aed36f2e0ebc71aa48fc609
mostmura <mustapha.aldaouk@gmail.com>
2024-02-20 14:25:00 +0100

    creation de la page pilulier

--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 // Route de base
 export default function Page() {
-  return <Text>Home page</Text>;
+  return (
+  <View>
+    <Text>Home page</Text>
+    <Link push href="/pilulier">Login</Link>
+
+  </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/pilulier.js
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import {Link} from "expo-router";
+
+
+export default function pilulier() {
+    return(
+        <View>
+            <Text>Test</Text>
+            <Link replace href="/index">retour</Link>
+        </View>
+
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
