be119fa6d33a1a0da719c8679bcf6278af74a077
Nyota0 <antoine.buret254@gmail.com>
2024-02-28 01:09:18 +0100

    debug navbar qui prend toute la page

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,15 @@
-import 'react-native-gesture-handler';
 import { Slot } from 'expo-router';
-import { Drawer } from 'expo-router/drawer';
 import Navbar from '../components/Navbar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Nous sert a garder la navbar et tout sur la page, seul changera ce qu'il y a dans <Slot />
 export default function HomeLayout() {
   return (
     <>
-    <Navbar />
-    <Slot />      
+      <SafeAreaView style = {{ flex: 1 }}>
+        <Navbar />
+        <Slot />
+      </SafeAreaView>    
     </>
   );
 }
