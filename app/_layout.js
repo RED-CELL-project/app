c4268161107b92799cd39b025fe76e3bd21bce78
mostmura <mustapha.aldaouk@gmail.com>
2024-02-28 11:45:55 +0100

    sidebar test
    dites moi si vous préférez cette approche, l'accueil sera un tableau de bord

--- app/_layout.js
+++ app/_layout.js
@@ -2,13 +2,96 @@ import 'react-native-gesture-handler';
 import { Slot } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
 import Navbar from '../components/Navbar';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome5';
+
 
 // Nous sert a garder la navbar et tout sur la page, seul changera ce qu'il y a dans <Slot />
 export default function HomeLayout() {
   return (
     <>
-    <Navbar />
-    <Slot />      
+    <Drawer>
+      <Drawer.Screen
+      name='index'
+      options={{
+        drawerLabel: 'Acceuil',
+        headerTitle: 'Acceuil',
+        drawerIcon: ({color, size}) => (
+          <Ionicons
+             name="home-outline" size={size} color={color}
+          />
+        )
+
+      }
+      }
+      />
+
+    <Drawer.Screen
+          name='pilulier'
+          options={{
+            drawerLabel: 'Pilulier',
+            headerTitle: 'Pilulier',
+            drawerIcon: ({color, size}) => (
+              <FontAwesome
+                 name="pills" size={size} color={color}
+              />
+            )
+          }
+          }
+          />
+    <Drawer.Screen
+          name='extraction_transmission'
+          options={{
+            drawerLabel: 'Extraction',
+            headerTitle: 'Extraction',
+            drawerIcon: ({color, size}) => (
+              <FontAwesome
+                 name="usb" size={size} color={color}
+              />
+            )
+          }
+          }
+          />
+    <Drawer.Screen
+          name='historique'
+          options={{
+            drawerLabel: 'Historique',
+            headerTitle: 'Historique',
+            drawerIcon: ({color, size}) => (
+              <Ionicons
+                 name="analytics-outline" size={size} color={color}
+              />
+            )
+          }
+          }
+          />
+    <Drawer.Screen
+          name='sign_in'
+          options={{
+            drawerLabel: 'Connexion',
+            headerTitle: 'Connexion',
+            drawerIcon: ({color, size}) => (
+              <Ionicons
+                 name="person-outline" size={size} color={color}
+              />
+            )
+          }
+          }
+          />
+    <Drawer.Screen
+          name='legal'
+          options={{
+            drawerLabel: 'Legal',
+            headerTitle: 'Legal',
+            drawerIcon: ({color, size}) => (
+              <FontAwesome
+                 name="balance-scale" size={size} color={color}
+              />
+            )
+          }
+          }
+          />
+    </Drawer>
     </>
   );
 }
