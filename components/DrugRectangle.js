c67662996864415094cd9910e8c5d514043b9362
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2024-03-26 15:21:07 +0100

    ajout page d'accueil

--- components/DrugRectangle.js
+++ components/DrugRectangle.js
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
+import color from '../styles/color';
 
 export default function DrugRectangle(drug) {
+    return (
+        <View style={styles.container}>
 
+        </View>
+    );
 }
 
-/*const styles = StyleSheet.create({
-
-});*/
+const styles = StyleSheet.create({
+    container:{
+        height: 100,
+        width: 150,
+        borderRadius: 5,
+        backgroundColor: color.secondary
+    }
+});
